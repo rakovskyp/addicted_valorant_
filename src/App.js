329bd227756971f2b6be30bdd671cdc53e73f7b7
Pesto <Pesto@deployuser21-MacBook-Air.local>
2020-06-29 21:54:17 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from "react";
+import axios from 'axios';
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
+
+const API_ENDPOINT = 'http://hn.algolia.com/api/v1/items/1';
+
+const getAsyncTime = () => 
+  new Promise((resolve, reject) => setTimeout(reject, 2000));
+
+const timeReducer = (state, action) => {
+  switch (action.type) {
+    case 'TIME_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'TIME_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'TIME_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+}
+
+function App() {
+  // useReducer Hook
+  const [time, dispatchTime] = React.useReducer(
+    timeReducer,
+    { data: [], isLoading: false, isError: false }
+  );
+
+  // useEffect Hook
+  
+
+  // Function to handle the submit button being clicked
+  const handleSubmit = (event) =>
+  {
+    console.log("You clicked Submit");
+
+    dispatchTime({type: 'TIME_FETCH_INIT'});
+    
+    axios
+      .get(`${API_ENDPOINT}`)
+      .then(result => {
+        dispatchTime({
+          type: 'TIME_FETCH_SUCCESS',
+          payload: result.data,
+        });
+      })
+      .catch(() =>
+        dispatchTime({ type: 'TIME_FETCH_FAILURE'} )
+      );
+
+    // Prevents browser from reloading once submit is clicked
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <div >
+        <h1 className="title" >Addicted to Valorant</h1>
+
+        <form className='search-box' onSubmit={handleSubmit}>
+          <input className='search-txt' type="text" id="search" placeholder='riot id n shit'/>
+          <a className='search-btn'>
+            <FaSearch/>
+          </a>
+          {/* <button 
+            type="submit">
+            Submit
+          </button> */}
+        </form>
+
+        {time.isError && <p>Something went wrong while fetching data</p>}
+
+        {time.isLoading ? (<p>Loading...</p>) : (<p>{time.data.type}</p>)}
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
